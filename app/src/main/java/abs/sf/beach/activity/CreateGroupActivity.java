eb41f885c64d85b74782749b2d61f16b4fba42b5
Ashutosh Srivastava <ashutosh0188@gmail.com>
2017-12-20 19:10:27 +0530

    Created create group ui

---
+++ app/src/main/java/abs/sf/beach/activity/CreateGroupActivity.java
@@ -0,0 +1,14 @@
+package abs.sf.beach.activity;
+
+import android.os.Bundle;
+
+import abs.sf.beach.android.R;
+
+public class CreateGroupActivity extends StringflowActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_group);
+    }
+}
