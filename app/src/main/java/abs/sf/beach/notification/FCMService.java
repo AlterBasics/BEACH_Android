ec092a2642ec6be7e5fc856e92cd4698865aa08e
Dharmu <dharmendra@stratawiz.com>
2017-12-30 15:03:34 +0530

    removing imprt error

--- app/src/main/java/abs/sf/beach/notification/FCMService.java
+++ app/src/main/java/abs/sf/beach/notification/FCMService.java
@@ -1,20 +1,9 @@
 package abs.sf.beach.notification;
 
-import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
-import java.util.Map;
-
-import abs.ixi.client.util.DateUtils;
-import abs.ixi.client.util.StringUtils;
-import abs.ixi.client.xmpp.InvalidJabberId;
-import abs.ixi.client.xmpp.JID;
-import abs.sf.beach.utils.NotificationUtils;
-import abs.sf.client.android.db.DbManager;
-import abs.sf.client.android.messaging.ChatLine;
-import abs.sf.client.android.messaging.Conversation;
 import abs.sf.client.android.notification.fcm.SFFcmService;
-import abs.sf.client.android.utils.SFNotifiactionCode;
+
 
 public class FCMService extends SFFcmService{
     private static final String TAG = FCMService.class.getSimpleName();
